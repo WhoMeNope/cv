5114ca6dbc0d691aef3d9fb9d9e3ed840e181203
Martin Toman <whomenope@outlook.com>
2019-07-26 19:24:57 +0200

    Timeline add Year marks

--- src/Timeline.js
+++ src/Timeline.js
@@ -2,9 +2,21 @@ import React from 'react';
 import './Timeline.css';
 
 function Timeline () {
+  const from = 2012
+  const to = 2019
+
+  const count = to - from + 1
+  const years = [...Array(count).keys()].map(year => year + from)
+
   return (
     <div className="Timeline">
-      Timeline
+      <div className="Years">
+      {
+        years.map(year => (
+          <div className="Year"> { year } </div>
+        ))
+      }
+      </div>
     </div>
   );
 }

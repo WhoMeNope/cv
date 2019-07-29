6725165758970b67751d10cc0d22446ff9e3c07d
Martin Toman <whomenope@outlook.com>
2019-07-29 16:32:34 +0200

    Timeline item primer

--- src/Timeline/Timeline.js
+++ src/Timeline/Timeline.js
@@ -3,6 +3,8 @@ import './Timeline.css'
 
 import Years from './Years'
 
+const barWidth = 20;
+
 function Timeline () {
   const from = 2012
   const to = 2019
@@ -10,9 +12,41 @@ function Timeline () {
   const count = to - from + 1
   const years = [...Array(count).keys()].map(year => year + from)
 
+  const content = [
+    {
+      date: {
+        from: new Date(2012, 8, 1),
+        to: new Date(2013, 8, 1),
+      },
+      title: "Item 1",
+    },
+    {
+      date: {
+        from: new Date(2012, 8, 1),
+        to: new Date(2013, 8, 1),
+      },
+      title: "Item 2",
+    },
+  ]
+
   return (
     <div className="Timeline">
       <Years from={from} to={to} />
+
+      <div className="Content">
+      {
+        content.map((item, index) => {
+          return (
+            <div className="Item" style={{
+              top: 100,
+              left: index * barWidth + (index + 1) * barWidth / 3,
+              height: '50%',
+            }}>
+            </div>
+          )
+        })
+      }
+      </div>
     </div>
   );
 }

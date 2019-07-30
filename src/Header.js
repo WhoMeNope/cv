07005a747c6e605c2352eae29d8548b027fa5f73
Martin Toman <whomenope@outlook.com>
2019-07-30 16:38:31 +0200

    Add info to Header

--- src/Header.js
+++ src/Header.js
@@ -2,9 +2,41 @@ import React from 'react';
 import './Header.css';
 
 function Header () {
+  const content = {
+    name: 'First Lastname',
+    phones: [
+      '202-555-0197',
+      '+1-202-555-0122',
+    ],
+    addresses: [
+      'name@company.com',
+      'www.website.com',
+    ],
+  }
+
   return (
     <div className="Header">
-      Header
+      <div className="Name">
+        { content.name }
+      </div>
+      <div className="Phones">
+      {
+        content.phones.map((phone, index) => (
+          <div key={index}>
+            { phone }
+          </div>
+        ))
+      }
+      </div>
+      <div className="Emails">
+      {
+        content.addresses.map((email, index) => (
+          <div key={index}>
+            { email }
+          </div>
+        ))
+      }
+      </div>
     </div>
   );
 }

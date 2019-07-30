4f4b775b4c205d8061f44556416d240df5851df0
Martin Toman <whomenope@outlook.com>
2019-07-30 18:38:32 +0200

    Format link (tel/mailto) + fix add keys to mapped list elements

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,25 @@
 import React from 'react';
 import './Header.css';
 
+function formatLink(input) {
+  function validateEmail(email) {
+    var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+  }
+  function validatePhone(inputtxt) {
+    var re = /^(\+?([0-9]{1,3})[-. ]?)?([0-9]{3})[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+    return re.test(inputtxt)
+  }
+
+  if (validateEmail(input)) {
+    return `mailto:${input}`
+  }
+  if (validatePhone(input)) {
+    return `tel:${input}`
+  }
+  return input
+}
+
 function Header () {
   const content = {
     name: 'First Lastname',
@@ -22,18 +41,22 @@ function Header () {
       <div className="Phones">
       {
         content.phones.map((phone, index) => (
+          <a href={formatLink(phone)} key={index}>
           <div key={index}>
             { phone }
           </div>
+          </a>
         ))
       }
       </div>
       <div className="Emails">
       {
         content.addresses.map((email, index) => (
+          <a href={formatLink(email)} key={index}>
           <div key={index}>
             { email }
           </div>
+          </a>
         ))
       }
       </div>

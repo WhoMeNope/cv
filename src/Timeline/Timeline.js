718e0f9a16902bb938fcea858e477db605e74e55
Martin Toman <whomenope@outlook.com>
2019-08-19 14:06:24 +0200

    Add order by category function

--- src/Timeline/Timeline.js
+++ src/Timeline/Timeline.js
@@ -16,8 +16,27 @@ function minMaxDates (content) {
   return { min, max }
 }
 
+function orderContent(content) {
+  let ordered = []
+
+  while (content.length) {
+    const pivot = content.shift()
+
+    ordered.push(pivot)
+    content.forEach((elem) => {
+      if (elem.category === pivot.category)
+        ordered.push(elem)
+    })
+    content = content.filter(elem => elem.category !== pivot.category)
+  }
+
+  return ordered
+}
+
 function Timeline ({ content }) {
   const { min, max } = minMaxDates(content)
+
+  // content = orderContent(content)
 
   const from = new Date(min.getFullYear(), 0)
   const to = new Date()

3a241cf3e9c92e989c8d3eae76b25677d5573fb7
Martin Toman <whomenope@outlook.com>
2019-09-17 20:58:09 +0200

    Add link to repo to Footer

--- src/Footer.js
+++ src/Footer.js
@@ -8,6 +8,12 @@ function Footer () {
         <span>Online version: </span>
         <a href={window.location.href}>{ window.location.href }</a>
       </div>
+      <div className="Link">
+        <span>Source (GPLv3): </span>
+        <a href="https://github.com/WhoMeNope/cv">
+          https://github.com/WhoMeNope/cv
+        </a>
+      </div>
     </div>
   )
 }
